f6af1e86c0953c17476277c4e405123f2aaf5721
chinamiii <chinami.2929@icloud.com>
2021-08-14 06:53:08 +0000

    [8/14]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,21 +12,28 @@ class BooksController < ApplicationController
   end
 
   def index
-  
+
     @newbook = Book.new
     @books = Book.all
-  
+    @user = current_user
+
   end
 
   def show
     @newbook = Book.new
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
+    @book = Book.find(params[:id])
+
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   end
   
   def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to books_path(@user.id)
   end
 
 
